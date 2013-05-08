6ee409dc82997a1143ec57161f7fe5458ea9b9b2
mstsai <b6683421@gmail.com>
2013-05-08 13:01:10 +0800

    CV HW2
    fixed bug

--- SIFT/imagewarp.cpp
+++ SIFT/imagewarp.cpp
@@ -19,15 +19,15 @@ Mat warp(Mat base,Mat warped,Mat H)
 	Mat points(3,1,CV_32F),transformed;
 	fprintf(fp,"width:%d height:%d\n",result.rows,result.cols);
 	for(int i=0;i<result.rows;i++){
-		for(int j=0;i<result.cols;j++){
+		for(int j=0;j<result.cols;j++){
 			points.at<float>(0,0)=(float)i;
 			points.at<float>(1,0)=(float)j;
 			points.at<float>(2,0)=1.0;
 			transformed=H*points;
 			transformed=transformed*(1/transformed.at<float>(2,0));
 			fprintf(fp,"x:%d y:%d u:%.3f v:%.3f ratio:%.3f\n",i,j,transformed.at<float>(0,0),transformed.at<float>(1,0),transformed.at<float>(2,0));
-			if((int)transformed.at<float>(0,0)<result.rows && (int)transformed.at<float>(1,0)<result.cols)
-				result.at<Vec3b>(i,j)=warped.at<Vec3b>((int)abs(transformed.at<float>(0,0)),(int)abs(transformed.at<float>(1,0)));
+			if(((int)transformed.at<float>(0,0)<result.rows && transformed.at<float>(0,0)>0.0) && ( (int)transformed.at<float>(1,0)<result.cols && transformed.at<float>(1,0)>0.0))
+				result.at<Vec3b>(i,j)=warped.at<Vec3b>((int)(transformed.at<float>(0,0)),(int)transformed.at<float>(1,0));
 			//result.at<Vec3b>(i,j)=
 		}
 	}
